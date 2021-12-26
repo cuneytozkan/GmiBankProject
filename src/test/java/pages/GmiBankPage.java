bac44bdc564cf332481841f029f0ac81421ca82e
Cuneyt <cnytozkn2014@gmail.com>
2021-12-26 11:17:48 +0100

    message

--- src/test/java/pages/GmiBankPage.java
+++ src/test/java/pages/GmiBankPage.java
@@ -3,7 +3,9 @@ package pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import utilities.Driver;
+import utilities.*;
+
+
 
 import java.util.WeakHashMap;
 
